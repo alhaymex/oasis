d258d79064b4c311240ab9fa531f0b0febf05439
Alharith Yassin <alhaymex@gmail.com>
2026-03-17 10:28:23 +0300

    Adding CEF to linux

--- apps/desktop/electrobun.config.ts
+++ apps/desktop/electrobun.config.ts
@@ -2,27 +2,29 @@ import type { ElectrobunConfig } from "electrobun";
 import pkgJSON from "./package.json";
 
 export default {
-	app: {
-		name: "Oasis",
-		identifier: "oasis.syferpool.com",
-		version: pkgJSON.version,
-	},
-	build: {
-		copy: {
-			"dist/index.html": "views/mainview/index.html",
-			"dist/assets": "views/mainview/assets",
-		},
-		// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-		// @ts-expect-error
-		watchIgnore: ["dist/**"],
-		mac: {
-			bundleCEF: false,
-		},
-		linux: {
-			bundleCEF: false,
-		},
-		win: {
-			bundleCEF: false,
-		},
-	},
+  app: {
+    name: "Oasis",
+    identifier: "oasis.syferpool.com",
+    version: pkgJSON.version,
+  },
+  build: {
+    copy: {
+      "dist/index.html": "views/mainview/index.html",
+      "dist/assets": "views/mainview/assets",
+    },
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
+    watchIgnore: ["dist/**"],
+    mac: {
+      bundleCEF: false,
+    },
+    linux: {
+      bundleCEF: true,
+      defaultRenderer: "cef",
+      bundleWGPU: true
+    },
+    win: {
+      bundleCEF: false,
+    },
+  },
 } satisfies ElectrobunConfig;
